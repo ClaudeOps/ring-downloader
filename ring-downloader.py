dfce962558d175b73d0a49583c41db8e41773f99
Claude Wilder <claude.wilder@gmail.com>
2020-02-04 18:17:25 -0500

    fix linting errors

--- ring-downloader.py
+++ ring-downloader.py
@@ -17,7 +17,6 @@ CONFIG_FILE = f'{CONFIG_DIR}/config.ini'
 PICKLE_FILE = f'{CONFIG_DIR}/ring-events.pickle'
 
 
-
 def download(doorbell, event):
     eventId = event.get('id')
     if eventId in downloaded_events:
@@ -34,7 +33,7 @@ def download(doorbell, event):
         if status == 'ready':
             try:
                 doorbell.recording_download(eventId, filename=filename)
-                os.utime(filename,(eventTime.timestamp(),eventTime.timestamp()))
+                os.utime(filename, (eventTime.timestamp(), eventTime.timestamp()))
                 return True
             except Exception as ex:
                 print(ex)
@@ -43,14 +42,18 @@ def download(doorbell, event):
             print(f'Event: {eventId} is {status}')
             return False
 
+
 cache_file = Path('token.cache')
+
 
 def token_updated(token):
     cache_file.write_text(json.dumps(token))
 
+
 def otp_callback():
     auth_code = input("2FA code: ")
     return auth_code
+
 
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
@@ -89,7 +92,7 @@ for doorbell in devices['doorbots']:
         print(f'Could not find time zone {timezone}. Setting to default timezone.')
         timezone = None
     for event in doorbell.history(limit=300, retry=10, timezone=timezone):
-        count +=1
+        count += 1
         if event.get('id') not in downloaded_events:
             if download(doorbell, event):
                 downloaded_events.append(event.get("id"))
@@ -101,6 +104,6 @@ for doorbell in devices['doorbots']:
             dlStatus = 'Skipping previous download'
 
     print(f'{doorbot} videos downloaded: {dlcount} out of {count}')
-    
+
 with open(PICKLE_FILE, 'wb') as handle:
     pickle.dump(downloaded_events, handle)
