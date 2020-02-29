dd105abab2c97dc6cd2e79575f5adaf6bd85e02b
Claude Wilder <claude.wilder@gmail.com>
2020-02-29 12:41:46 -0500

    improve exception handling

--- ring-downloader.py
+++ ring-downloader.py
@@ -5,6 +5,7 @@ import json
 import os
 import pickle
 from pathlib import Path
+import sys
 import pytz
 from oauthlib.oauth2 import MissingTokenError
 from ring_doorbell import Ring, Auth
@@ -14,79 +15,86 @@ CONFIG_FILE = f"{CONFIG_DIR}/config.ini"
 PICKLE_FILE = f"{CONFIG_DIR}/ring-events.pickle"
 CACHE_FILE = Path(f"{CONFIG_DIR}/token.cache")
 
+CONFIG = configparser.ConfigParser()
+CONFIG.read(CONFIG_FILE)
+DOWNLOADFOLDER = CONFIG["DEFAULT"]["video_directory"]
+USERNAME = CONFIG["DEFAULT"]["user_name"]
+PASSWORD = CONFIG["DEFAULT"]["password"]
 
-def download(doorbell, event):
-    eventId = event.get("id")
-    if eventId in downloaded_events:
+
+def download(bell, evnt):
+    event_id = evnt.get("id")
+    if event_id in downloaded_events:
         return True
 
-    doorbot = doorbell.name
-    eventTime = event.get("created_at")
+    event_time = evnt.get("created_at")
     filename = "".join(
         (
-            f"{downloadFolder}/",
-            f"{doorbot}-",
-            f'{eventTime.strftime("%Y%m%d_%H%M%S")}-',
-            f"{eventId}.mp4",
+            f"{DOWNLOADFOLDER}/",
+            f"{bell.name}-",
+            f'{event_time.strftime("%Y%m%d_%H%M%S")}-',
+            f"{event_id}.mp4",
         )
     )
     filename = filename.replace(" ", "_")
 
     print(filename)
-    status = event.get("recording", {}).get("status")
+    status = evnt.get("recording", {}).get("status")
 
     if status == "ready":
         try:
-            doorbell.recording_download(eventId, filename=filename)
-            os.utime(filename, (eventTime.timestamp(), eventTime.timestamp()))
+            bell.recording_download(event_id, filename=filename)
+            os.utime(
+                filename, (event_time.timestamp(), event_time.timestamp())
+            )
             return True
         except Exception as ex:
             print(ex)
             return False
     else:
-        print(f"Event: {eventId} is {status}")
+        print(f"Event: {event_id} is {status}")
         return False
 
 
 def token_updated(token):
+    """
+        Cache updated token to token file for next run
+    """
     CACHE_FILE.write_text(json.dumps(token))
 
 
 def otp_callback():
+    """
+        Get 2FA code from user
+    """
     auth_code = input("2FA code: ")
     return auth_code
 
 
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE)
-
 if CACHE_FILE.is_file():
-    auth = Auth("RDL/0.1", json.loads(CACHE_FILE.read_text()), token_updated)
+    AUTH = Auth("RDL/0.1", json.loads(CACHE_FILE.read_text()), token_updated)
 else:
-    USERNAME = config["DEFAULT"]["user_name"]
-    PASSWORD = config["DEFAULT"]["password"]
-    auth = Auth("RDL/0.1", None, token_updated)
+    AUTH = Auth("RDL/0.1", None, token_updated)
     try:
-        auth.fetch_token(USERNAME, PASSWORD)
+        AUTH.fetch_token(USERNAME, PASSWORD)
     except MissingTokenError:
-        auth.fetch_token(USERNAME, PASSWORD, otp_callback())
+        AUTH.fetch_token(USERNAME, PASSWORD, otp_callback())
 
-myring = Ring(auth)
+myring = Ring(AUTH)
 myring.update_data()
-downloadFolder = config["DEFAULT"]["video_directory"]
 
 try:
     with open(PICKLE_FILE, "rb") as handle:
         downloaded_events = pickle.loads(handle.read())
-except Exception as ex:
-    print(f"Error opening pickle file")
-    print(ex)
+except FileNotFoundError:
+    print(f"Download history file missing. No problem. We'll create a new one when we're finished.")
     downloaded_events = []
+except BaseException:
+    print("Something went wrong opening the download history file.")
+    sys.exit(1)
 
-devices = myring.devices()
-
-# output = ''
-for doorbell in devices["doorbots"]:
+DEVICES = myring.devices()
+for doorbell in DEVICES["doorbots"]:
     dlcount = 0
     count = 0
     doorbot = doorbell.name
@@ -97,7 +105,7 @@ for doorbell in devices["doorbots"]:
             "Setting to default timezone.",
         )
         timezone = None
-    for event in doorbell.history(limit=300, retry=10, timezone=timezone):
+    for event in doorbell.history(limit=100, retry=10, timezone=timezone):
         count += 1
         if event.get("id") not in downloaded_events:
             if download(doorbell, event):
